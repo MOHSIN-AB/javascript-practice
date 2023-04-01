192fddba478f8df21caee24f2c08a99fd2006601
mohsin raza abid <125199140+mohsin-abi@users.noreply.github.com>
2023-04-01 19:56:08 +0500

    Add files via upload

---
+++ javascript2/index6.js
@@ -0,0 +1,10 @@
+
+ let numbers = [1,2,3,4,5];
+ const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+ console.log(sum);
+
+
+ accumulator + currentValue :return;
+ 1000     ,          2000    ,        3000
+3000       ,        10000   ,         13000
+13000       ,        500    ,        13500
---
+++ javascript2/index7.js
@@ -0,0 +1,4 @@
+accumulator + currentValue = return;
+1000     ,          2000    ,        3000
+3000       ,        10000   ,         13000
+13000       ,        500    ,        13500
