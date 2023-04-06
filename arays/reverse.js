a29c626be15b4f324fbc11333e5fd02fda09054f
mohsin raza abid <125199140+mohsin-abi@users.noreply.github.com>
2023-04-06 23:01:05 +0500

    Add files via upload

---
+++ arays/reverse.js
@@ -0,0 +1,9 @@
+let arr = [1,2,3,4,5,6,7,8]
+console.log(arr.reverse());
+
+
+// ya
+
+
+// let arr = [8,7,6,5,4,3,2,1]
+// console.log(arr.reverse());
---
+++ arays/unshift.js
@@ -0,0 +1,2 @@
+let arr = [2,3,4,"hi i am mohsin raza abid",5,6,]
+console.log(arr.unshift());
